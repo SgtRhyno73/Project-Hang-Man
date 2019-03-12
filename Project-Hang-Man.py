ea4e7ba0ba3a4256f0764e6cab8e5b6a0e0b7d6e
keegan <43976092+SgtRhyno73@users.noreply.github.com>
2019-03-12 07:39:02 -0400

    add guess on hangman

--- Project-Hang-Man.py
+++ Project-Hang-Man.py
@@ -187,3 +187,36 @@ def main():
             print(joined_word)
 
             try:
+                player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
+            except: # check valid input
+                print("That is not valid input. Please try again.")
+                continue                
+            else: 
+                if not player_guess.isalpha(): # check the input is a letter. Also checks an input has been made.
+                    print("That is not a letter. Please try again.")
+                    continue
+                elif len(player_guess) > 1: # check the input is only one letter
+                    print("That is more than one letter. Please try again.")
+                    continue
+                elif player_guess in guessed_letters: # check it letter hasn't been guessed already
+                    print("You have already guessed that letter. Please try again.")
+                    continue
+                else:
+                    pass
+
+            guessed_letters.append(player_guess)
+
+            for letter in range(len(chosen_word)):
+                if player_guess == chosen_word[letter]:
+                    word_guessed[letter] = player_guess # replace all letters in the chosen word that match the players guess
+
+            if player_guess not in chosen_word:
+                attempts -= 1
+                print(HANGMAN[(len(HANGMAN) - 1) - attempts])
+
+        if "-" not in word_guessed: # no blanks remaining
+            print(("\nCongratulations! {} was the word").format(chosen_word))
+        else: # loop must have ended because attempts reached 0
+            print(("\nUnlucky! The word was {}.").format(chosen_word))
+
+        print("\nWould you like to play again?")
