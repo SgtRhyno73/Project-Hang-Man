f109d0afccf9642636ce810bfdd8ec42773ba9af
Keegan Killian <02keegan.killian@gulllakecs.org>
2019-03-12 09:27:05 -0400

    finished the code and will add numbers   maybe

--- Project-Hang-Man.py
+++ Project-Hang-Man.py
@@ -46,7 +46,7 @@ def main():
         joined_word = None  # joins the words in the list word_guessed
 
         HANGMAN = (
-'''
+"""
 ------
 |    |
 |
@@ -56,30 +56,30 @@ def main():
 |
 |
 ---------
-''',
-'''
-------
-|    |
-|   ( )
-|
-|
-|
-|
-|
----------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
+|
+|
+|
+|
+|
+---------
+""",
+"""
+------
+|    |
+|   ( )
 |    |
 |
 |
 |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
@@ -89,94 +89,95 @@ def main():
 |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |
 |
 |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |    |
 |
 |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |    |
 |   |
 |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |    |
 |   |
 |   |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |    |
 |   | |
 |   |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |    |
 |   | |
 |   | |
 |
 ---------
-''','''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |  | | 
 |   | |
 |   | |
 |
 ---------
-''',
-'''
+""",
+"""
 ------
 |    |
 |   ( )
-|  /-+-\
+|  /-+-\ 
 |  | | |
 |   | |
 |   | |
 |
 ---------
-''')
+""")
 
         print(HANGMAN[0])
         attempts = len(HANGMAN) - 1
@@ -184,19 +185,19 @@ def main():
 
         while (attempts != 0 and "-" in word_guessed):
             print(("\nYou have {} attempts remaining").format(attempts))
-            joined_word = "".joined(word_guessed)
+            joined_word = "".join(word_guessed)
             print(joined_word)
 
             try:
                 player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
             except:  # check valid input
                 print("That is not valid input. Please try again.")
-                continue                
-            else: 
+                continue
+            else:
                 if not player_guess.isalpha():  # check the input is a letter. Also checks an input has been made.
                     print("That is not a letter. Please try again.")
                     continue
-                elif len(player_guess) > 1: # check the input is only one letter
+                elif len(player_guess) > 1:  # check the input is only one letter
                     print("That is more than one letter. Please try again.")
                     continue
                 elif player_guess in guessed_letters:  # check it letter hasn't been guessed already
@@ -209,8 +210,7 @@ def main():
 
             for letter in range(len(chosen_word)):
                 if player_guess == chosen_word[letter]:
-                    word_guessed[letter] = player_guess
-                    #  replace all letters in the chosen word that match the players guess
+                    word_guessed[letter] = player_guess  #  replace all letters in the chosen word that match the players guess
 
             if player_guess not in chosen_word:
                 attempts -= 1
@@ -218,13 +218,14 @@ def main():
 
         if "-" not in word_guessed: # no blanks remaining
             print(("\nCongratulations! {} was the word").format(chosen_word))
-        else: # loop must have ended because attempts reached 0
+        else:  # loop must have ended because attempts reached 0
             print(("\nUnlucky! The word was {}.").format(chosen_word))
 
         print("\nWould you like to play again?")
+
         response = input("> ").lower()
         if response not in ("yes", "y"):
             play_again = False
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
